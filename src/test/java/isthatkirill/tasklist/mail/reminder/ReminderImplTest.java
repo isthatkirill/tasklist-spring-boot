39955ba77a24666eb9c175fb293f8521ddd397e4
isthatkirill <kirill.emelyanoff2021@yandex.ru>
2023-11-03 23:45:53 +0300

    feat: tests for Reminder added

---
+++ src/test/java/isthatkirill/tasklist/mail/reminder/ReminderImplTest.java
@@ -0,0 +1,58 @@
+package isthatkirill.tasklist.mail.reminder;
+
+import isthatkirill.tasklist.mail.service.MailService;
+import isthatkirill.tasklist.task.model.Task;
+import isthatkirill.tasklist.task.service.TaskService;
+import isthatkirill.tasklist.user.model.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.Properties;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Kirill Emelyanov
+ */
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+class ReminderImplTest {
+
+    @MockBean
+    private TaskService taskService;
+
+    @MockBean
+    private MailService mailService;
+
+    private Task task = Task.builder()
+            .title("title")
+            .description("description")
+            .status("DONE")
+            .owner(User.builder().id(1L).build())
+            .build();
+
+    @Autowired
+    private Reminder reminder;
+
+    @Test
+    void remindTest() {
+        when(taskService.getAllSoonTasks(any())).thenReturn(List.of(task));
+        Properties properties = new Properties();
+        properties.setProperty("task.title", task.getTitle());
+        properties.setProperty("task.description", task.getDescription());
+        properties.setProperty("task.status", task.getStatus());
+
+        reminder.remind();
+
+        verify(mailService).sendEmail(eq(task.getOwner()), any(), eq(properties));
+    }
+}
